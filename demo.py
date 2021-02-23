7cd9bd5832d745bbe97e0b39b3108fab4ca27d83
rafa0805 <theryk0805@gmail.com>
2021-02-23 09:22:38 +0900

    update: path settings of outputs

--- demo.py
+++ demo.py
@@ -2,14 +2,14 @@ import myTests
 
 browsers = [
   # "Chrome",
-  # "Firefox",
-  "Edge"
+  "Firefox"
+  # "Edge"
 ]
 
 cases = [
-  {"case_name" : "case1", "campaign_code" : "132", "name_sei" : "テスト", "name_mei" : "ネーム", "customer_code1" : "1111", "customer_code2" : "222222", "customer_code3" : "3", "zip1" : "100", "zip2" : "0004"},
-  {"case_name" : "case2", "campaign_code" : "132", "name_sei" : "テスト", "name_mei" : "ネーム", "customer_code1" : "1111", "customer_code2" : "222222", "customer_code3" : "3", "zip1" : "100", "zip2" : "0004"},
-  {"case_name" : "case3", "campaign_code" : "132", "name_sei" : "テスト", "name_mei" : "ネーム", "customer_code1" : "1111", "customer_code2" : "222222", "customer_code3" : "3", "zip1" : "100", "zip2" : "0004"}
+  {"case_name" : "test1", "search" : "Selenium1"},
+  {"case_name" : "test2", "search" : "Selenium2"},
+  {"case_name" : "test3", "search" : "Selenium3"}
 ]
 
 for browser in browsers:
--- myTests.py
+++ myTests.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 import json
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -11,12 +12,16 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class WebTest():
 
-  # *************** クラスインタフェース ***************
-  def __init__(self, cases, browser):
+  # *************** Class Interface ***************
+  def __init__(self, cases, browser, output="./output/"):
+    # set params
     self.cases = cases
     self.browser = browser
+    self.output_path = output
+    # set up dirs
+    self.create_output_dirs()
+    # start up driver
     self.start_driver(self.browser)
-    self.save_path = "./screenshots/"
 
   def execute(self):
     count = 0
@@ -29,22 +34,20 @@ class WebTest():
 
   def teardown_method(self, method):
     self.driver.quit()
+
+  def create_output_dirs(self):
+    os.makedirs(self.output_path + "screenshots/", mode=0o777, exist_ok=True)
+    os.makedirs(self.output_path + "logs/", mode=0o777, exist_ok=True)
  
 
 
-  # *************** メイン処理 ***************
+  # *************** Main Process ***************
   def flow(self, case, last_flag):
-    self.driver.get("https://www.astomos-retailing.com/order/")
-    self.driver.find_element(By.ID, "campaign_code").send_keys(case["campaign_code"])
-    self.driver.find_element(By.ID, "name_sei").send_keys(case["name_sei"])
-    self.driver.find_element(By.ID, "name_mei").send_keys(case["name_mei"])
-    self.driver.find_element(By.ID, "customer_code1").send_keys(case["customer_code1"])
-    self.driver.find_element(By.ID, "customer_code2").send_keys(case["customer_code2"])
-    self.driver.find_element(By.ID, "customer_code3").send_keys(case["customer_code3"])
-    self.driver.find_element(By.ID, "zip1").send_keys(case["zip1"])
-    self.driver.find_element(By.ID, "zip2").send_keys(case["zip2"])
-    time.sleep(1)    
-    self.full_screen(self.save_path + self.browser + "_" + case["case_name"] + "_a")
+    self.driver.get("https://www.google.com/")
+    self.driver.find_element(By.CSS_SELECTOR, "input[name='q']").send_keys(case["search"])
+    time.sleep(1)
+    filename = self.output_path + "screenshots/" + self.browser + "_" + case["case_name"] + "_a"
+    self.full_screen(filename)
 
     if last_flag == 1:
       self.driver.close()
@@ -54,7 +57,7 @@ class WebTest():
 
 
 
-  # *************** 小分け関数群 ***************
+  # *************** Utilities ***************
   def full_screen(self, filename):
     w = self.driver.execute_script("return document.body.scrollWidth")
     h = self.driver.execute_script("return document.body.scrollHeight")
@@ -63,7 +66,7 @@ class WebTest():
 
 
 
-  # *************** ドライバ関連の関数群 ***************
+  # *************** Setting up Drivers ***************
   def start_driver(self, browser):
     if browser == "Firefox":
       self.firefox_driver()
@@ -89,7 +92,7 @@ class WebTest():
     from selenium.webdriver.firefox.options import Options
     self.options = Options()
     self.options.add_argument('-headless')
-    self.driver = webdriver.Firefox(firefox_options=self.options)
+    self.driver = webdriver.Firefox(firefox_options=self.options, log_path=self.output_path + "logs/geckodriver.log")
 
   def edge_driver(self):
     # from selenium.webdriver.edge.options import Options
